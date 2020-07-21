c4c5d2f366c361ac8717543f656e9424c9394e6c
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-07-21 18:16:53 +0530

    Log In & Register Screens

--- Messanger/Controllers/ConversationViewController.swift
+++ Messanger/Controllers/ConversationViewController.swift
@@ -12,9 +12,21 @@ class ConversationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "login_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
 
 }
 
